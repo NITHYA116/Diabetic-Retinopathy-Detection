9548438a1ab799e12330f68d1be6ceab0340909f
Balakrishna K <nithyajetty116@gmail.com>
2023-04-24 00:24:03 +0530

    app.py update

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import img_to_array
 import streamlit as st
 import numpy as np
+from PIL import Image
+import io
 
 # Load your saved model
 model = tf.keras.models.load_model('my_model.h5')
@@ -23,9 +25,27 @@ def predict_image(image):
 
 
 def app():
-    st.set_page_config(page_title='Image Classification App')
-    st.title('Image Classification App')
+    # Set page title and favicon
+    st.set_page_config(
+        page_title='Diabetic Retinopathy Classification App', page_icon=':eye:', layout='wide')
+
+    # Set up the sidebar
+    st.sidebar.title('About')
+    st.sidebar.info(
+        'This app uses a deep learning model to classify diabetic retinopathy.')
+    st.sidebar.info(
+        'Upload an image of an eye to classify whether the patient has diabetic retinopathy or not.')
+    st.sidebar.info(
+        'The model was trained on the Kaggle Diabetic Retinopathy dataset, which contains over 35,000 images.')
+    st.sidebar.info(
+        'To learn more about the dataset, go to https://www.kaggle.com/c/diabetic-retinopathy-detection/overview')
+
+    # Set up the main content area
+    st.title('Diabetic Retinopathy Classification App')
+    st.image('logo.png', width=200)
+    st.markdown('---')
     st.write('Upload an image for classification')
+    st.markdown('---')
 
     # Create a file uploader
     uploaded_file = st.file_uploader(
@@ -33,21 +53,35 @@ def app():
 
     if uploaded_file is not None:
         # Display the uploaded image
-        image_to_classify = image.load_img(uploaded_file)
+        image_to_classify = Image.open(io.BytesIO(uploaded_file.read()))
         st.image(image_to_classify, caption='Uploaded image',
                  use_column_width=True)
 
         # Make predictions on the image
         predictions = predict_image(image_to_classify)
         # Replace with your own class names
-        class_names = ['No_DR', 'DR']
-        top_k = 2  # Display top-3 predictions
+        class_names = ['No DR', 'DR']
+        top_k = 2  # Display top-2 predictions
         classes = np.argsort(predictions[0])[::-1][:top_k]
         probs = predictions[0][classes]
 
-        st.write('Top-{} Predictions:'.format(top_k))
-        for i in range(top_k):
-            st.write('{}: {:.2%}'.format(class_names[classes[i]], probs[i]))
+        # Create a 2-column layout
+        col1, col2 = st.beta_columns(2)
+
+        # Display the top-k predictions in the left column
+        with col1:
+            st.write('Top-{} Predictions:'.format(top_k))
+            for i in range(top_k):
+                st.write('{}: {:.2%}'.format(
+                    class_names[classes[i]], probs[i]))
+            st.markdown('---')
+
+        # Display a bar chart of the predictions in the right column
+        with col2:
+            chart_data = {'class': [class_names[c]
+                                    for c in classes], 'probability': probs}
+            st.bar_chart(chart_data)
+            st.markdown('---')
 
 
 if __name__ == '__main__':
